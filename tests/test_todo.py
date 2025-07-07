d860a208862872a94bf1e93e7f304b15d4b32c99
Chris Jewell <chrisjewell@heidihealth.com>
2025-07-08 08:08:40 +1000

    TCR: 08:08:40

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -4,3 +4,8 @@
 def test_true_is_truthy() -> None:
     """Test that true is truthy."""
     assert True
+
+
+def test_can_import_core() -> None:
+    """Test that we can import from tcr_todo.core."""
+    import tcr_todo.core
