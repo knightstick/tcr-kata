017bf2ca97f7977f790814bee324270fcb8a4aae
Chris Jewell <chrisjewell@heidihealth.com>
2025-07-08 08:36:37 +1000

    TCR: 08:36:37

--- src/tcr_todo/cli.py
+++ src/tcr_todo/cli.py
@@ -1,8 +1,10 @@
 """Command line interface for tcr_todo."""
 
+import argparse
 from typing import Union
 
 
 def main(args: Union[list[str], None] = None) -> None:
     """Main CLI entry point."""
+    parser = argparse.ArgumentParser()
     pass
