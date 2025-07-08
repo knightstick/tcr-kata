09e2a9ac54c7d50b39fa5de0b33cf34ed45119b2
Chris Jewell <chrisjewell@heidihealth.com>
2025-07-08 10:08:21 +1000

    TCR: 10:08:21

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,6 +1,6 @@
 """Tests for todo functionality."""
 
-from tcr_todo.core import add_todo, Todo
+from tcr_todo.core import add_todo, _list_todos, Todo
 
 
 def test_add_todo_creates_todo_with_title() -> None:
@@ -20,3 +20,14 @@ def test_todo_has_string_representation() -> None:
     todo = Todo("buy milk")
     result = str(todo)
     assert "buy milk" in result
+
+
+def test_list_todos_with_fake_repo() -> None:
+    """Test that _list_todos uses the provided retrieve function."""
+    expected_todos = [Todo("task 1"), Todo("task 2")]
+
+    def fake_retrieve() -> list[Todo]:
+        return expected_todos
+
+    result = _list_todos(fake_retrieve)
+    assert result == expected_todos
