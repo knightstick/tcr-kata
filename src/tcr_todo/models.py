801c27b92d39297f86320ba7a193220e0b109670
Chris Jewell <chrisjewell@heidihealth.com>
2025-07-08 10:12:39 +1000

    TCR: 10:12:39

--- src/tcr_todo/models.py
+++ src/tcr_todo/models.py
@@ -11,3 +11,15 @@ class Todo:
     def __str__(self) -> str:
         """Return string representation of todo."""
         return self.title
+
+
+class TodoList:
+    """A container for todo items that knows how to display itself."""
+
+    def __init__(self, todos: list[Todo]) -> None:
+        """Initialize with a list of todos."""
+        self.todos = todos
+
+    def __str__(self) -> str:
+        """Return formatted string representation of the todo list."""
+        return str([str(todo) for todo in self.todos])
