bddbd31e3dd30705c123930ee6068165bf86b110
Chris Jewell <chrisjewell@heidihealth.com>
2025-07-08 10:01:18 +1000

    TCR: 10:01:18

--- src/tcr_todo/repo.py
+++ src/tcr_todo/repo.py
@@ -9,3 +9,8 @@ _todos: list[Todo] = []
 def store_todo(todo: Todo) -> None:
     """Store a todo item."""
     _todos.append(todo)
+
+
+def retrieve_todos() -> list[Todo]:
+    """Retrieve all stored todos."""
+    return _todos.copy()
