8ad8cf6952fe23953d425e035071eac8b010c1c4
Chris Jewell <chrisjewell@heidihealth.com>
2025-07-08 10:25:10 +1000

    TCR: 10:25:10

--- src/tcr_todo/repo.py
+++ src/tcr_todo/repo.py
@@ -14,3 +14,19 @@ def store_todo(todo: Todo) -> None:
 def retrieve_todos() -> list[Todo]:
     """Retrieve all stored todos."""
     return _todos.copy()
+
+
+class InMemoryRepo:
+    """In-memory todo repository."""
+
+    def __init__(self) -> None:
+        """Initialize with empty todo list."""
+        self._todos: list[Todo] = []
+
+    def store_todo(self, todo: Todo) -> None:
+        """Store a todo item."""
+        self._todos.append(todo)
+
+    def retrieve_todos(self) -> list[Todo]:
+        """Retrieve all stored todos."""
+        return self._todos.copy()
